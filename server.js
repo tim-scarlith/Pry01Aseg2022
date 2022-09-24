6ab29990def3f39e2df25d20189209956027fcf3
tim-scarlith <53355647+tim-scarlith@users.noreply.github.com>
2022-09-24 18:03:43 -0500

    Update server.js
    space change 02

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
